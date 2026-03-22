9335e4993a775993df0eb620e63378cefaf4a1f7
Chakravarthy Peela <chakripeela@gmail.com>
2026-03-22 17:35:32 +0530

    remove express

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Starting deployment..."
-echo "Installing npm dependencies..."
-npm ci
-
-echo "Dependency installation complete!"
-echo "App deployment ready. Express server will start with 'npm start' command."
